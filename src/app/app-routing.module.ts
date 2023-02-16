4acbe343b784f06dcc3b942df0dd3df183b1289f
Dmytro Ostretsov <1654045@gmail.com>
2023-02-16 20:29:56 +0200

    bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './modules/container/container.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ContainerComponent,
+},
+{ path: '404', component: NotFoundComponent },
+{ path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import apiConfig from '../config/default.json';
+
+interface Config {
+  apiUrl: string;
+}
+
+export default apiConfig as Config;
